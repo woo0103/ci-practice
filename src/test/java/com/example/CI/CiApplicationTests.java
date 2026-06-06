240f36de7b50cf244f3e79e13bca74718d1a0631
wooooo <playjiwoo@naver.com>
2026-06-06 21:04:13 +0900

    first commit

---
+++ src/test/java/com/example/CI/CiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
